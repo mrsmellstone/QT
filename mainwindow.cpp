8452397178aa5bd5e303f22ee27fd6292ebf8b4d
iprod12.ru <iprod12@MacBook-Pro-xxx.local>
2019-05-09 16:39:36 +0300

    project uploaded

---
+++ mainwindow.cpp
@@ -0,0 +1,72 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    //Рисуем график y=x*x
+
+    //Сгенерируем данные
+    //Для этого создадим два массива точек:
+    //один для созранения x координат точек,
+    //а второй для y соответственно
+    double Xm = (ui->lineEdit_3->text()).toDouble();
+    double W = (ui->lineEdit_2->text()).toDouble();
+    double f0 = (ui->lineEdit->text()).toDouble();
+    double a = -10;
+    //Начало интервала, где рисуем график по оси Ox
+    double b =  10; //Конец интервала, где рисуем график по оси Ox
+    double h = 0.01; //Шаг, с которым будем пробегать по оси Ox
+
+    int N=(b-a)/h + 2; //Вычисляем количество точек, которые будем отрисовывать
+    QVector<double> x(N), y(N); //Массивы координат точек
+
+    //Вычисляем наши данные
+    int i=0;
+    for (double X=a; X<=b; X+=h)//Пробегаем по всем точкам
+    {
+        x[i] = X;
+        y[i] = Xm*sin(W*X + f0);//Формула нашей функции
+        i++;
+    }
+
+    ui->widget->clearGraphs();//Если нужно, но очищаем все графики
+    //Добавляем один график в widget
+    ui->widget->addGraph();
+    //Говорим, что отрисовать нужно график по нашим двум массивам x и y
+    ui->widget->graph(0)->setData(x, y);
+
+    //Подписываем оси Ox и Oy
+    ui->widget->xAxis->setLabel("x");
+    ui->widget->yAxis->setLabel("y");
+
+    //Установим область, которая будет показываться на графике
+    ui->widget->xAxis->setRange(a, b);//Для оси Ox
+
+    //Для показа границ по оси Oy сложнее, так как надо по правильному
+    //вычислить минимальное и максимальное значение в векторах
+    double minY = y[0], maxY = y[0];
+    for (int i=1; i<N; i++)
+    {
+        if (y[i]<minY) minY = y[i];
+        if (y[i]>maxY) maxY = y[i];
+    }
+    ui->widget->yAxis->setRange(minY, maxY);//Для оси Oy
+
+    //И перерисуем график на нашем widget
+    ui->widget->replot();
+}
